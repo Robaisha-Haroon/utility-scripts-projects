d31ecc1ea649bc0ad9c6b64f9248687d023ef3d6
Robaisha Haroon <robaishaharoon2010@gmail.com>
2026-09-12 22:38:37 +0500

    Create Simple_Calculator.py
    In this program, I created a simple calculator that takes 2 inputs from the user and performs operations +, -, *, and /.

---
+++ Simple_Calculator.py
@@ -0,0 +1,15 @@
+# Simple Calculator
+
+# Enter Numbers
+num1= int(input("Enter 1st number:"))
+num2= int(input("Enter 2nd number:"))
+# Performing tasks in variable
+add= num1 + num2
+minus= num1 - num2
+multi= num1* num2
+divide= num1/num2
+# Printing results
+print(f"the sum of {num1} and {num2} is {add}")
+print(f"the subtraction of {num1} and {num2} is {minus}")
+print(f"the product of {num1} and {num2} is {multi}")
+print(f"the division of {num1} and {num2} is {divide}")
